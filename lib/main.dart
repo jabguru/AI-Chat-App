c657b796a7bae3192fdb9b557182d16115ff43e5
Alibrown Julius <brownjulius980@gmail.com>
2025-11-03 00:30:54 +0100

    chore: organized imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
+import 'package:ai_chat_app/core/theme/theme.dart';
 import 'package:ai_chat_app/features/auth/presentation/auth_wrapper.dart';
 import 'package:ai_chat_app/features/auth/presentation/welcome.dart';
 import 'package:ai_chat_app/features/chat/presentation/chat_screen.dart';
 import 'package:ai_chat_app/shared/services/supabase_service.dart';
-import 'package:ai_chat_app/core/theme/theme.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   await dotenv.load(fileName: ".env");
   await SupabaseService.initialize();
-  
+
   runApp(const ProviderScope(child: MyApp()));
 }
 
